57a992460cd18c084ab6325b72168e9aef24d636
Dylan Vidal <dylanvidal1204@gmail.com>
2024-09-29 00:00:11 -0400

    Add the title dialog

--- src/app/_components/voice.tsx
+++ src/app/_components/voice.tsx
@@ -8,9 +8,17 @@ import { api } from "~/trpc/react";
 import { convertToExcalidrawElements } from "@excalidraw/excalidraw";
 import { type ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 import { parseMermaidToExcalidraw } from "@excalidraw/mermaid-to-excalidraw";
-import { Eraser, Mic, MicOff, Pencil } from "lucide-react";
+import { Eraser, Loader2Icon, Mic, MicOff, Pencil } from "lucide-react";
 import dynamic from "next/dynamic";
 import { toast } from "~/hooks/use-toast";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./../../components/ui/dialog";
 
 const Excalidraw = dynamic(
   async () => (await import("@excalidraw/excalidraw")).Excalidraw,
@@ -24,11 +32,16 @@ const VoiceDraw = () => {
   const [filteredTranscript, setFilteredTranscript] = useState("");
   const [mermaid, setMermaid] = useState("");
   const [exAPI, setExAPI] = useState<ExcalidrawImperativeAPI | null>(null);
+  const [dialogOpen, setDialogOpen] = useState<boolean>(true);
 
   const [gemInput, setGemInput] = useState<string>(
     "Please generate a rectangle with text that says Hello World",
   );
-  const { refetch: getMermaid, data } = api.mermaid.toMer.useQuery(
+  const {
+    refetch: getMermaid,
+    data,
+    isLoading,
+  } = api.mermaid.toMer.useQuery(
     { str: gemInput, current: mermaid },
     { enabled: false },
   );
@@ -76,13 +89,14 @@ const VoiceDraw = () => {
       // Convert the transcript to an array of words (lowercase and without punctuation)
       const words = transcript
         .split(" ")
-        .map((word) => word.toLowerCase().replace(/[^a-zA-Z ]/g, ""));
+        .map((word) => word.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, ""));
       console.log(words);
 
       // Check if "lets draw" appears in sequence in the words array
       for (let i = 0; i < words.length - 1; i++) {
         if (words[i] === "lets" && words[i + 1] === "draw") {
           // Filter the transcript from the point of "lets" (inclusive) and store it
+          setDialogOpen(false);
           const filteredTranscript = words.slice(i).join(" ");
           setFilteredTranscript(filteredTranscript);
           break; // Stop once the first occurrence is found
@@ -162,6 +176,26 @@ const VoiceDraw = () => {
 
   return (
     <div className="items between h-full w-full flex-col items-center">
+      <Dialog open={dialogOpen}>
+        <DialogContent className="max-w-xl">
+          <DialogHeader>
+            <DialogTitle className="text-2xl font-bold">
+              Welcome to VoiceBoard
+            </DialogTitle>
+            <DialogDescription className="text-xl font-semibold">
+              Making whiteboarding and communicating ideas easier and accessible
+              to all.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="text-2xl">
+            Say{" "}
+            <span className="text-purple-700">
+              &quot;Let&apos;s Draw&quot;{" "}
+            </span>
+            to get started...
+          </div>
+        </DialogContent>
+      </Dialog>
       <div className="flex w-full flex-row px-12 py-8 text-center">
         <div
           className="cursor-pointer px-2 text-3xl font-bold"
@@ -172,10 +206,17 @@ const VoiceDraw = () => {
           }
         >
           <div
-            className={`p-2 ${listening ? "rounded-full bg-red-800" : "rounded bg-violet-800"}`}
+            className={`relative p-2 ${listening ? "rounded-full bg-red-800" : "rounded bg-violet-800"}`}
             onClick={() => setMermaid("graph TD")}
           >
-            {listening ? <Mic /> : <MicOff />}
+            {listening ? (
+              <div>
+                <Mic />
+                <span className="absolute left-0 top-0 inline-flex h-full w-full animate-ping rounded-full bg-red-700 opacity-75"></span>
+              </div>
+            ) : (
+              <MicOff />
+            )}
           </div>
         </div>
         <div className="flex cursor-pointer flex-row px-2 italic">
@@ -196,22 +237,28 @@ const VoiceDraw = () => {
           </div>
           <div className="ml-4 mt-2">&quot;let&apos;s draw&quot;</div>
         </div>
+        {isLoading && (
+          <div className="flex cursor-pointer flex-row px-2 italic">
+            <div className="p-2" onClick={() => setMermaid("graph TD")}>
+              <Loader2Icon className="animate-spin" />
+            </div>
+            <div className="ml-4 mt-2">loading...</div>
+          </div>
+        )}
       </div>
       <div className="px-12">
-        <div className="h-[70vh] w-full rounded-xl border-4">
+        <div className="relative h-[85vh] w-full rounded-xl border-4">
           <Excalidraw excalidrawAPI={(api) => setExAPI(api)} />
+          <div className="flex flex-grow items-center justify-center px-12 pt-8 text-black">
+            {filteredTranscript ? (
+              <p className="animate-grow absolute bottom-4 z-10 rounded-xl bg-zinc-900 p-4 text-center text-xl text-white opacity-40">
+                <div className="opacity-100">{filteredTranscript}</div>
+              </p>
+            ) : (
+              <div />
+            )}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-grow items-center justify-center px-12 pt-8">
-        {filteredTranscript ? (
-          <p className="animate-grow rounded-xl border-2 border-slate-900 bg-slate-200 p-4 text-center text-xl text-slate-800">
-            {filteredTranscript}
-          </p>
-        ) : !listening ? (
-          <div>Listening is currently disabled.</div>
-        ) : (
-          <div />
-        )}
       </div>
     </div>
   );
